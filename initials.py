e16ee716751c671d8728aa9a1d8917facbe6fe5c
ADEGU KELVIN K.K <2425402460@live.gctu.edu.gh>
2026-05-30 11:07:35 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints stylized initials KAKK using simple print statements.
+# Assignment Number: 1
+#
+# Name: KELVIN ADEGU KORLETEY KORBLAH
+# STUDENT ID: 2425402460
+# Email:2425402460@live.gctu.edu.gh
+# Grader:Augustus Buckman
+
+#
+# On my honor, KELVIN ADEGU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    """Prints the small initials and stylized large block letters."""
+    print()
+    print("...KAKK")
+    print()
+    print("...K....K...   ...   AAAAAA   ...   ...K....K...   ....")
+    print("...K...K....   ...  AAAAAAAA  ...   ...K...K....   ....")
+    print("...K..K.....   ...  AA....AA  ...   ...K..K.....   ....")
+    print("...K.K......   ...  AA....AA  ...   ...K.K......   ....")
+    print("...KK.......   ...  AAAAAAAA  ...   ...KK.......   ....")
+    print("...KK.......   ...  AAAAAAAA  ...   ...KK.......   ....")
+    print("...K.K......   ...  AA....AA  ...   ...K.K......   ....")
+    print("...K..K.....   ...  AA....AA  ...   ...K..K.....   ....")
+    print("...K...K....***...  AA....AA  ...***...K...K....***..")
+    print("...K....K...***...  AA....AA  ...***...K....K...***.")
+    print()
 
 
 main()
